74ea39869080d9137f6429ee0d87066e0a7e31d4
Yug1020 <yugandhardhore@gmail.com>
2025-10-27 20:20:31 +0530

    just change the text of navbar and sidebar of admin panel

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,7 +4,12 @@ import {assets} from '../assets/assets'
 const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
-        <h1 className='text-2xl font-bold'>FurniVerse</h1>
+      <span className="font-bold">
+        Mentor
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#e67e22] to-[#f39c12]">
+          Map
+        </span>
+      </span>
         <button onClick={()=>setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
