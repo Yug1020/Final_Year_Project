19ada9c90bb80fa1faaefbc069661780d698f870
Yug1020 <143815412+Yug1020@users.noreply.github.com>
2025-12-05 22:47:21 +0530

    Update backend URL in ShopContext

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,7 +10,7 @@ const ShopContextProvider = (props) =>
 
     const currency = '₹';
     const delivery_fee = 10;
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://mentormap-qb93.onrender.com"
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
